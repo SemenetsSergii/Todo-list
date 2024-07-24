e502b2fcb588c5b9f156b97e8cdc9d5ddedf9a2a
Sergii Semenets <serhey.semenets@gmail.com>
2024-07-24 11:53:12 +0300

    create models Tag & Task in models.py

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    date_create = models.DateTimeField()
+    deadline = models.DateField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.content
+
