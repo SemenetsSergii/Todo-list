c9450c2d703c9a2f335e27f5b18d50d769e87ce7
Sergii Semenets <serhey.semenets@gmail.com>
2024-07-24 13:45:05 +0300

    add TaskUpdateView  & TaskDeleteView, create task delete.html

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from todo.views import TaskListView, TaskCreateView
+from todo.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+
+)
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task-list'),
-    path("", TaskCreateView.as_view(), name='task-create'),
-]
+    path("task/create/", TaskCreateView.as_view(), name='task-create'),
+    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete")
+    ]
 
 app_name = 'todo'
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from todo.models import Task
 from todo.forms import TaskForm
@@ -13,5 +14,20 @@ class TaskListView(generic.ListView):
 class TaskCreateView(generic.CreateView):
     model = Task
     form_class = TaskForm
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todoapp/task_form.html"
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+
 
 
